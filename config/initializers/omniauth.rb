67f671945c2e897ba050c86c949800b0af83bfe6
Omer <momeriqbal2@gmail.com>
2014-02-19 05:45:47 +0800

    fixed fb app ids

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 OmniAuth.config.logger = Rails.logger
+if Rails.env.development? 
+    fb_app_id = "608873879182214"
+    fb_secret = '2a7d62b84fd3febc1b3ba32a505e996e'
+else
+    fb_app_id =  "1387251161495606" 
+    fb_secret = "31e217dadb8ed0ea0c43aa09d07f63bf"
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '608873879182214', '2a7d62b84fd3febc1b3ba32a505e996e'
+  provider :facebook, fb_app_id , fb_secret 
 end
